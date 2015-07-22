4f428057d3500a861560cb540b3d4196c66d62e1
Johannes Kulick <kulick@hildensia.de>
2015-07-22 10:55:39 +0200

    use redis for data handling

---
+++ devviz/data_handling.py
@@ -0,0 +1,48 @@
+import redis
+from devviz import app
+
+class DataHandler:
+    def __init__(self):
+        self._r = redis.StrictRedis(decode_responses=True)
+        self._r.flushdb()
+        self._pubsub = self._r.pubsub()
+
+    def get_variables(self):
+        return self._r.lrange("variables", 0, -1)
+
+    def get_type(self, variable):
+        return self._r.get("type_{}".format(variable))
+
+    def get_values(self, variable):
+        return self._r.lrange(variable, 0, -1)
+
+    def get_value(self, variable, idx=0):
+        return self._r.lindex(variable, idx)
+
+    def get_view_vars(self, viewid):
+        return self._r.lrange("view-{}".format(viewid), 0, -1)
+
+    def set_view(self, viewid, view):
+        self._r.set("view_type_{}".format(viewid), view.url)
+
+    def get_view(self, viewid):
+        view_name = self._r.get("view_type_{}".format(viewid))
+        return app.views[view_name]()
+
+    def subscribe(self, var):
+        self._pubsub.subscribe(var)
+
+    def listen(self):
+        return self._pubsub.listen()
+
+    def add_view_var(self, viewid, var):
+        self._r.lpush("view-{}".format(viewid), var)
+
+    def del_view_var(self, viewid, var):
+        self._r.lrem(viewid, 0, var)
+
+    def get_new_viewid(self):
+        return self._r.incr("views")
+
+
+
---
+++ devviz/static/js/variablesview.jsx
@@ -0,0 +1,59 @@
+/**
+ * Created by johannes on 7/17/15.
+ */
+
+console.log("LOADED");
+var VariableTable = React.createClass({
+    getInitialState: function() {
+        return {data: []};
+    },
+    componentWillMount: function(){
+        var stream = new EventSource("/variables/stream");
+        stream.addEventListener("message", this.process, false);
+    },
+    process: function(e) {
+        var payload = JSON.parse(e.data);
+        this.setState({data: payload.data});
+    },
+    componentDidMount: function() {
+        $.ajax({
+            url: this.props.url,
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                this.setState({data: data.data});
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    render: function() {
+        var variableNodes = this.state.data.map(function (variable) {
+            return (
+                <Variable name={variable.name} type={variable.type} />
+            );
+        });
+        return (
+            <table className="table table-striped">
+                <thead>
+                <tr><th>Name</th><th>Type</th></tr>
+                </thead>
+                <tbody>
+                {variableNodes}
+                </tbody>
+            </table>
+        );
+    }
+});
+var Variable = React.createClass({
+    render: function() {
+        return (
+            <tr><td>{this.props.name}</td><td>{this.props.type}</td></tr>
+        );
+    }
+});
+React.render(
+    <VariableTable url="/variables" />,
+    document.getElementById('variablesview')
+);
