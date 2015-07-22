efea65d0f43720ad2c05796507d203c4812acc5e
Johannes Kulick <kulick@hildensia.de>
2015-07-22 10:57:47 +0200

    save active views (still buggy)

--- devviz/app.py
+++ devviz/app.py
@@ -1,12 +1,15 @@
 __author__ = 'johannes'
-from flask import render_template
-from .views import BokehLinechart, VariablesView, TableView
+from flask import render_template, session
 from devviz import app
 
 
 @app.route('/')
 def main_page():
-    return render_template("devviz.html",
-                           views=[VariablesView(),
-                                  TableView(variables=['y', 'z']),
-                                  BokehLinechart(['y'])])
+    try:
+        active_views = session['active_views']
+    except KeyError:
+        session['active_views'] = []
+        active_views = []
+    views = app.views.values()
+    return render_template("devviz.html", views=views,
+                           active_views=active_views)
