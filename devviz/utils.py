86a8f1f37bf33922850401860160a64a813a4ffe
Johannes Kulick <kulick@hildensia.de>
2015-07-22 10:59:48 +0200

    sse_route for convenience

--- devviz/utils.py
+++ devviz/utils.py
@@ -1,1 +1,24 @@
 __author__ = 'johannes'
+
+from flask import request, Response, redirect
+from functools import wraps
+
+
+def sse_route(function):
+    '''
+    A decorator that wraps a flask route to be a server-sent event stream.
+    The decorated function has to yield json strings.
+    :param function:
+    :return:
+    '''
+    def yielding_function(*args, **kwargs):
+        for value in function(*args, **kwargs):
+            yield "data: {}\n\n".format(value)
+
+    @wraps(function)
+    def sse_function(*args, **kwargs):
+        if request.headers.get('accept') == 'text/event-stream':
+            return Response(yielding_function(*args, **kwargs),
+                            content_type='text/event-stream')
+        return next(yielding_function(*args, **kwargs))
+    return sse_function
