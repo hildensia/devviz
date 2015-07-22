d5f5e00c30656b4566671cf41603493499be1161
Johannes Kulick <kulick@hildensia.de>
2015-07-22 10:58:20 +0200

    View as base class for views

--- devviz/views/views.py
+++ devviz/views/views.py
@@ -1,22 +1,76 @@
 __author__ = 'johannes'
 
-from devviz import redis_store
+from devviz import data_handler, app
+from devviz.utils import sse_route
+
+from collections import namedtuple
+
+from flask import jsonify, session
 import json
+import time
+
+Variable = namedtuple("Variable", ("name", "type", "value"))
+Variable.__new__.__defaults__ = (None,)
 
 
 class View(object):
-    def __init__(self, variables, viewid=None):
+    script = ''
+    style = ''
+
+    def __init__(self, variables=None, viewid=None):
+        if variables is None:
+            variables = []
+
         if viewid is None:
-            self.viewid = redis_store.incr('views')
-            redis_store.set("view-"+str(self.viewid), json.dumps(variables))
-            print("set")
-            self.variables = variables
+            self.viewid = data_handler.get_new_viewid()
+            for var in variables:
+                data_handler.add_view_var(self.viewid, var)
 
         else:
             self.viewid = viewid
-            self.variables = redis_store.get(viewid)
 
-    def __del__(self):
-        redis_store.delete(self.viewid)
-        redis_store.decr('views')
+    def add_var(self, variable):
+        data_handler.add_view_var(self.viewid, variable)
+        return jsonify({'content': self.content})
+
+    def del_var(self, variable):
+        data_handler.del_view_var(self.viewid, variable)
+        return jsonify({'content': self.content})
+
+    @property
+    def variables(self):
+        return [Variable(var, data_handler.get_type(var),
+                         data_handler.get_value(var))
+                for var in data_handler.get_view_vars(self.viewid)]
+
+
+@app.route("/views/new/<name>")
+def new_view(name):
+    view = app.views[name]()
+    data_handler.set_view(view.viewid, view)
+    session['active_views'].append(view.viewid)
+    return jsonify({"content": view.content})
+
+
+@app.route("/views/<viewid>/add/<var>")
+def add_var(viewid, var):
+    view = data_handler.get_view(viewid)
+    return view.add_var(var)
+
+
+@app.route("/views/<viewid>/del/<var>")
+def del_var(viewid, var):
+    view = data_handler.get_view(viewid)
+    return view.del_var(var)
+
+
+@app.route("/views/<viewid>/data")
+@sse_route
+def data_stream(viewid):
+    while True:
+        time.sleep(.1)
+        for var in data_handler.get_view_vars(viewid):
+            yield json.dumps({"value": data_handler.get_value(var),
+                              "var": var})
+
 
